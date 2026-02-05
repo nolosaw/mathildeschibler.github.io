3c2a81a1d2ce0913dfab3895a9721db0ec6ad439
unetouriste <mathschibler@gmail.com>
2026-02-05 17:13:33 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.filters button');
+    const tags = document.querySelectorAll('.filters .tag');
     const projects = document.querySelectorAll('.project');
   
-    buttons.forEach(button => {
-      button.addEventListener('click', () => {
-        const tag = button.getAttribute('data-tag');
+    tags.forEach(tag => {
+      tag.addEventListener('click', () => {
+        // Gérer la sélection/dé-sélection des tags
+        const isSelected = tag.classList.contains('selected');
+        tags.forEach(t => t.classList.remove('selected')); // Réinitialiser tous les tags
+        if (!isSelected) {
+          tag.classList.add('selected'); // Ajouter la classe "selected" au tag cliqué
+        }
+  
+        // Filtrer les projets
+        const selectedTag = tag.getAttribute('data-tag');
         projects.forEach(project => {
-          if (tag === 'all' || project.getAttribute('data-tags').includes(tag)) {
+          if (selectedTag === 'all' || project.getAttribute('data-tags').includes(selectedTag)) {
             project.style.display = 'block';
           } else {
             project.style.display = 'none';
